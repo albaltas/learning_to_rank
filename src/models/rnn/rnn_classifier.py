b038e9068bf7d8cfc0fe5b1c0f1658bae5313fe8
Simon Stiebellehner <simon.stiebellehner@gmail.com>
2017-04-19 21:57:36 +0100

    added link

--- src/models/rnn/rnn_classifier.py
+++ src/models/rnn/rnn_classifier.py
@@ -3,6 +3,7 @@
 Created on Sun Feb 26 19:56:37 2017
 
 @author: Simon Stiebellehner
+https://github.com/stiebels
 
 This file implements a Multi-layered (stacked), bidirectional Recurrent Neural Network for classification,
 followed by two feed-forward ReLu layers and a softmax function for multiclass-classification.
